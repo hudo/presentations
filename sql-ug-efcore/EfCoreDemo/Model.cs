b1644249df13a065745de467faa83a731f901cfb
Hrvoje <hrvoje.hudoletnjak@gmail.com>
2017-07-30 21:47:56 +0100

    added model and some queries

---
+++ sql-ug-efcore/EfCoreDemo/Model.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Remotion.Linq.Parsing.Structure.ExpressionTreeProcessors;
+
+namespace EfCoreDemo
+{
+    public class ConferenceContext : DbContext
+    {
+        public ConferenceContext()
+        {
+            
+        }
+
+        public ConferenceContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Conference> Conferences { get; set; }
+        public DbSet<Talk> Talks { get; set; }
+        public DbSet<Presenter> Presenters { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=conferences;Trusted_Connection=true");
+            //optionsBuilder.UseSqlite("DataSource=:memory:");
+        }
+    }
+
+    public class Conference
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<Talk> Talks { get; set; }
+    }
+
+    public class Talk
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Presenter Presenter { get; set; }
+        public Conference Conference { get; set; }
+    }
+
+    public class Presenter
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- sql-ug-efcore/EfCoreDemo/Program.cs
+++ sql-ug-efcore/EfCoreDemo/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace EfCoreDemo
@@ -7,7 +13,61 @@ namespace EfCoreDemo
     {
         static void Main(string[] args)
         {
-            
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<ConferenceContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            var db = new ConferenceContext(options);
+            db.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+
+            db.Database.EnsureCreated();
+
+            Seed(db);
+
+            Dump(db.Talks.Include(x => x.Presenter).ToList());
+
+            var conf = db.Conferences.First();
+            db.Entry(conf).Collection(x => x.Talks).Load();
+            Dump(conf);
+
+            Dump(db.Talks.Select(x => new
+            {
+                x.Id,
+                Presenter = x.Presenter.Name,
+                Title = x.Conference.Title,
+            }));
+
+            Console.WriteLine("done.");
+        }
+
+
+        static void Seed(ConferenceContext db)
+        {
+            if (!db.Conferences.Any())
+            {
+                db.Conferences.Add(new Conference
+                {
+                    Title = "Sql Ug Dublin",
+                    Talks = new List<Talk>
+                    {
+                        new Talk
+                        {
+                            Title = "first talk",
+                            Presenter = new Presenter {Name = "presenter 1"}
+                        },
+                        new Talk
+                        {
+                            Title = "second talk",
+                            Presenter = new Presenter {Name = "presenter 2"}
+                        }
+                    }
+                });
+
+                db.SaveChanges();
+            }
         }
 
         static void Dump(object item)
